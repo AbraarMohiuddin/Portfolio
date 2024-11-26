14a268170c47a6e5177346cadae1f4bb5ce68a18
Abraar Mohiuddin <abraarmohiuddin3@outlook.com>
2024-11-26 18:27:37 +1100

    project restructure

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -16,7 +16,7 @@ const Projects = () => {
       description:
         "An AI-powered career development platform that provides interactive quizzes using dynamic content. Built as part of Industry Project with Qeeri.AI, using React.js, ASP.NET Web API, Flask, and Azure. Features include AI-driven quiz questions, personalized feedback, and user authentication with ASP.NET Identity.",
       image: northstar,
-      github: "/northstar",
+      github: "https://youtu.be/3kOSUTqk1aw",
       live: true,
     },
     {
